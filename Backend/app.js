71497f981bf0d4418c1a4927894ebbf809f49cf4
Manas Tamboli <manastamboli2@gamail.com>
2024-09-25 20:52:23 +0530

    last done

--- Backend/app.js
+++ Backend/app.js
@@ -5,11 +5,12 @@ const cors = require('cors')
 const cookie = require('cookie-parser')
 // const bodyParser = require('body-parser')
 // app.use(bodyParser)
-// app.use(cors({
-//     origin: 'https://freelanze-frontend-74k8.onrender.com', // Your frontend origin
-//     credentials: true // Allow credentials (cookies)
-// }));
+app.use(cors({
+    origin: 'https://freelanze-frontend-74k8.onrender.com', // Your frontend origin
+    credentials: true // Allow credentials (cookies)
+}));
 app.use(cors());
+app.options('*', cors()); // Enable preflight across all routes
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookie())
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,17 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// vite.config.js
-export default {
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://freelanze-backend.onrender.com', // Backend API URL in development
-        changeOrigin: true
-      }
-    }
-  }
-}
+// // vite.config.js
+// export default {
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://freelanze-backend.onrender.com', // Backend API URL in development
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
