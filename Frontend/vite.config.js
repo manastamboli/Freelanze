8eb5c6179869568d6a73eb3c00a60b20ec2aaae3
Manas Tamboli <manastamboli2@gamail.com>
2024-09-25 20:49:14 +0530

    config done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+// vite.config.js
+export default {
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://freelanze-backend.onrender.com', // Backend API URL in development
+        changeOrigin: true
+      }
+    }
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
