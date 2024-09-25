8f51c2ee6a10dbed35b3baeed67301c1c2604ac2
Manas Tamboli <manastamboli2@gamail.com>
2024-09-25 20:56:59 +0530

    app changed

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,22 @@ const cors = require('cors')
 const cookie = require('cookie-parser')
 // const bodyParser = require('body-parser')
 // app.use(bodyParser)
-app.use(cors({
-    origin: 'https://freelanze-frontend-74k8.onrender.com', // Your frontend origin
-    credentials: true // Allow credentials (cookies)
-}));
-app.use(cors());
-app.options('*', cors()); // Enable preflight across all routes
+// app.use(cors({
+//     origin: 'https://freelanze-frontend-74k8.onrender.com', // Your frontend origin
+//     credentials: true // Allow credentials (cookies)
+// }));
+// app.use(cors());
+// app.options('*', cors()); // Enable preflight across all routes
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://freelanze-frontend-74k8.onrender.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    next();
+  });
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cookie())
